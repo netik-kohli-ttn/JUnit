314086e1022f87ed38eebb7ee0ca8e98e3359360
netik-kohli-ttn <netik.kohli@tothenew.com>
2025-02-20 09:56:07 +0530

    Update EmailServiceTest.java

--- unit-testing/src/test/java/com/demo/service/EmailServiceTest.java
+++ unit-testing/src/test/java/com/demo/service/EmailServiceTest.java
@@ -10,25 +10,26 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class EmailServiceTest {
     private static EmailService emailService;
-    private static Order order;
 
     @BeforeClass
-    public static void InitBeforeClass() throws Exception {
-         emailService = new EmailService();
-         order = new Order(1, "Smart Phone", 200000);
+    public static void initialization() {
+        emailService = new EmailService();
     }
 
     @Test
-    public void sendEmail() {
+    public void testGetInstance() {
+        Assert.assertNotNull(EmailService.getInstance());
+    }
+
+    @Test
+    public void testSendEmail() {
+        Order order = new Order(1, "Sandwich", 150);
         Assert.assertTrue(emailService.sendEmail(order, "netik@gmail.com"));
     }
-    @Test
-    public void sendEmailWithException() {
-        try {
-            emailService.sendEmail(order);
-            throw new AssertionError("RuntimeException was not thrown");
-        } catch (RuntimeException e) {
-            Assert.assertFalse(order.isCustomerNotified());
-        }
+
+    @Test(expected = RuntimeException.class)
+    public void testSendEmailException() {
+        Order order = new Order(2, "Burger", 200);
+        emailService.sendEmail(order);
     }
 }
